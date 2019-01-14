7fe8c5d1007807f7fc3bd39d7a6ea4b2b1a0cd44
Robbie Baker <robbiethebakerman@googlemail.com>
2019-01-14 19:12:35 +0000

    FilmList

---
+++ src/components/FilmLink.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilmLink extends Component {
+
+  render() {
+    return (
+      <p>Film</p>
+    );
+  }
+
+}
+
+export default FilmLink;
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import FilmLink from './FilmLink';
 
 class FilmList extends Component {
 
   render() {
+
+    const filmLinkNodes = this.props.data.map(filmLink => {
+      return <FilmLink key={filmLink.id} url={filmLink.url}>{filmLink.name}</FilmLink>
+    });
+
     return (
-      <p>Film</p>
+      <div className="film-list">
+        {filmLinkNodes}
+      </div>
     );
+
   }
 
 }
